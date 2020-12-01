2b6755eeee1e61287ac22e0ee155bc13bceaaac3
lauren raddatz <lauren.raddatz@workday.com>
2020-12-01 10:44:51 -0700

    day 1

--- src/dec01/index.js
+++ src/dec01/index.js
@@ -15,14 +15,13 @@ const twoSumAndMultiply = (arr) => {
 }
 
 const threeSumAndMultiply = (arr) => {
-  const hash = {}
   const target = 2020
   // fuck it brute force
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
       for (let k = 0; k < arr.length; k++) {
         if (arr[i] + arr[j] + arr[k] === target) {
-          return arr[i] * arr[j] * arr[k];
+          return arr[i] * arr[j] * arr[k]
         }
       }
     }
