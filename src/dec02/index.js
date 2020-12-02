45079035ee5e9f8e9c81bd9ffd06ba4b16a712a4
lauren raddatz <lauren.raddatz@workday.com>
2020-12-01 22:41:19 -0700

    day 2

---
+++ src/dec02/index.js
@@ -0,0 +1,41 @@
+import { readFileSync } from 'fs' 
+
+const input = readFileSync('src/dec02/input.txt').toString()
+const data = input.split('\n')
+
+const countValidPasswords1 = (policies) => {
+  let count = 0
+  policies.map((policy) => {
+    const [rule, letterColon, password] = policy.split(' ')
+    const [min, max] = rule.split('-')
+    const letter = letterColon.substring(0, 1)
+    const re = new RegExp(letter, 'g')
+    const letterCount = password.includes(letter) ? password.match(re).length : -1
+    if (letterCount >= min && letterCount <= max) {
+      count++
+    }
+  })
+  return count
+}
+
+const countValidPasswords2 = (policies) => {
+  let count = 0
+  policies.map((policy) => {
+    const [rule, letterColon, password] = policy.split(' ')
+    const letter = letterColon.substring(0, 1)
+    const [firstIndex, secondIndex] = rule.split('-').map((n) => n - 1)
+    if (password[firstIndex] === letter) {
+      if (password[secondIndex] !== letter) {
+        count++
+      }
+    } else if (password[secondIndex] === letter) {
+      if (password[firstIndex] !== letter) {
+        count++
+      }
+    }
+  })
+  return count
+}
+
+console.log('part one: ', countValidPasswords1(data))
+console.log('part two: ', countValidPasswords2(data))
