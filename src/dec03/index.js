518281c801a792592017792b175d91ee1a007bb3
lauren raddatz <lauren.raddatz@workday.com>
2020-12-03 12:12:42 -0700

    day 3

--- src/dec03/index.js
+++ src/dec03/index.js
@@ -7,46 +7,21 @@ const countHowManyTreesYouHit = (treeMap) => {
   // increase by an arbitrarily large number
   const extendedTreeMap = treeMap.map((line) => line.repeat(100))
 
-  let index1 = 0
-  let treeCount1 = 0
-  extendedTreeMap.map((line) => {
-    line[index1] === '#' ? treeCount1++ : undefined
-    index1 += 1
-  })
+  const getTreeCount = (dx, dy) => {
+    let index = 0
+    let treeCount = 0
+    extendedTreeMap.map((line, i) => {
+      if (i % dy === 0) {
+        line[index] === '#' ? treeCount++ : undefined
+        index += dx
+      }
+    })
+    return treeCount
+  }
 
-  let index2 = 0
-  let treeCount2 = 0
-  extendedTreeMap.map((line) => {
-    line[index2] === '#' ? treeCount2++ : undefined
-    index2 += 3
-  })
-
-  let index3 = 0
-  let treeCount3 = 0
-  extendedTreeMap.map((line) => {
-    line[index3] === '#' ? treeCount3++ : undefined
-    index3 += 5
-  })
-
-  let index4 = 0
-  let treeCount4 = 0
-  extendedTreeMap.map((line) => {
-    line[index4] === '#' ? treeCount4++ : undefined
-    index4 += 7
-  })
-
-  let index5 = 0
-  let treeCount5 = 0
-  extendedTreeMap.map((line, i) => {
-    // even indices only
-    if (i % 2 === 0) {
-      line[index5] === '#' ? treeCount5++ : undefined
-      index5 += 1
-    }
-  })
-
-  console.log('part one ', treeCount2)
-  console.log('part two ', treeCount1 * treeCount2 * treeCount3 * treeCount4 * treeCount5)
+  console.log('part one ', getTreeCount(3, 1))
+  const slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+  console.log('part two ', slopes.reduce((acc, slope) => acc * getTreeCount(slope[0], slope[1]), 1))
 }
 
 countHowManyTreesYouHit(data)
